0bf6d1ded26c943de281473a048b9311befa882a
Paul Melnikow <email@paulmelnikow.com>
2018-11-01 20:05:32 -0400

    [DEPLOY] Completely clean the index

--- services/test-helpers.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const colorscheme = require('../lib/colorscheme.json')
-const mapValues = require('lodash.mapvalues')
-
-module.exports = {
-  colorScheme: mapValues(colorscheme, 'colorB'),
-}
